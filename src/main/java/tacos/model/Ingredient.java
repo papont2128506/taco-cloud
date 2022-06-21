59e08677687a2a60f75554797ad45296f172595f
trusov <a.trusov@voskhod.ru>
2022-06-21 19:02:20 +0400

    Added design controller

---
+++ src/main/java/tacos/model/Ingredient.java
@@ -0,0 +1,11 @@
+package tacos.model;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+}
---
+++ src/main/java/tacos/model/Type.java
@@ -0,0 +1,9 @@
+package tacos.model;
+
+public enum Type {
+    WRAP,
+    PROTEIN,
+    VEGGIES,
+    CHEESE,
+    SAUCE
+}
